10f591409c9447d01ea1130feea572193987d40a
Pierre Neidhardt <ambrevar@gmail.com>
2014-11-05 18:42:09 +0100

    Remove alignment in indentation engine
    The main reason for this change is to change indentation from
    
      foobar('arg1', function ()
               print('foobar')
                     end)
    
    to
    
      foobar('arg1', function ()
          print('foobar')
      end)
    
    The indentation engine is very convoluted, hence the major changes.
    
    The new indentation algorithm is as follows:
    Current line indentation is:
    + indentation changes induced by unmatched closers on current line
    + indentation changes induced by previous line
    + previous line indentation
    - indent-level if previous line was a continuation
    - indent-level
    
    Implementation changes:
    * Keyword 'function' is in the same group as '{', '[' and '('.
    * Openers yield a simple indent, no alignment.
    * Closers at the beginning of the line do not yield any indent for following line.
    * Continuing lines are not indented together, only previous line is used for indentation.
    
    As a result, the engine never scans back more than the previous line, except to
    check for matching openers. This should result in a sensible performance boost.

--- lua-mode.el
+++ lua-mode.el
@@ -1211,33 +1211,16 @@ The criteria for a continuing statement are:
   "This is a helper function to lua-calculate-indentation-info. Don't
 use standalone."
   (cond
-   ;; function is a bit tricky to indent right. They can appear in a lot ot
-   ;; different contexts. Until I find a shortcut, I'll leave it with a simple
-   ;; relative indentation.
-   ;; The special cases are for indenting according to the location of the
-   ;; function. i.e.:
-   ;;       (cons 'absolute (+ (current-column) lua-indent-level))
-   ;; TODO: Fix this. It causes really ugly indentations for in-line functions.
-   ((string-equal found-token "function")
+   ;; Block openers.
+   ((member found-token (list "{" "(" "[" "function"))
     (cons 'relative lua-indent-level))
-
-   ;; block openers
-   ((member found-token (list "{" "(" "["))
-    (save-excursion
-      (let ((found-bol (line-beginning-position)))
-        (forward-comment (point-max))
-        ;; If the next token is on this line and it's not a block opener,
-        ;; the next line should align to that token.
-        (if (and (zerop (count-lines found-bol (line-beginning-position)))
-                 (not (looking-at lua-indentation-modifier-regexp)))
-            (cons 'absolute (current-column))
-          (cons 'relative lua-indent-level)))))
 
    ;; These are not really block starters. They should not add to indentation.
    ;; The corresponding "then" and "do" handle the indentation.
    ((member found-token (list "if" "for" "while"))
     (cons 'relative 0))
-   ;; closing tokens follow: These are usually taken care of by
+
+   ;; Closing tokens follow: These are usually taken care of by
    ;; lua-calculate-indentation-override.
    ;; elseif is a bit of a hack. It is not handled separately, but it needs to
    ;; nullify a previous then if on the same line.
@@ -1249,28 +1232,37 @@ use standalone."
             (cons 'remove-matching 0)
           (cons 'relative 0)))))
 
-   ;; else is a special case; if its matching block token is on the same line,
+   ;; 'else' is a special case; if its matching block token is on the same line,
    ;; instead of removing the matching token, it has to replace it, so that
    ;; either the next line will be indented correctly, or the end on the same
    ;; line will remove the effect of the else.
    ((string-equal found-token "else")
-     (save-excursion
-       (let ((line (line-number-at-pos)))
-         (if (and (lua-goto-matching-block-token found-pos 'backward)
-                  (= line (line-number-at-pos)))
-             (cons 'replace-matching (cons 'relative lua-indent-level))
-                   (cons 'relative lua-indent-level)))))
-
-   ;; Block closers. If they are on the same line as their openers, they simply
-   ;; eat up the matching indentation modifier. Otherwise, they pull
-   ;; indentation back to the matching block opener.
-   ((member found-token (list ")" "}" "]" "end"))
     (save-excursion
       (let ((line (line-number-at-pos)))
-        (lua-goto-matching-block-token found-pos 'backward)
-        (if (/= line (line-number-at-pos))
-            (lua-calculate-indentation-info (point))
-          (cons 'remove-matching 0)))))
+        (if (and (lua-goto-matching-block-token found-pos 'backward)
+                 (= line (line-number-at-pos)))
+            (cons 'replace-matching (cons 'relative lua-indent-level))
+          (cons 'relative lua-indent-level)))))
+
+   ;; Block closers. If they are on the same line as their openers, they simply
+   ;; eat up the matching indentation modifier. Otherwise, they pull indentation
+   ;; back to the matching block opener. Special case if the token is at
+   ;; beginning of line: since `lua-calculate-indentation-override' unindented
+   ;; the line by one level already, then it only eats up the matching
+   ;; indentation midifier.
+   ((member found-token (list ")" "}" "]" "end"))
+    (save-excursion
+      (let ((line (line-number-at-pos))
+            (cur-point (point)))
+        (back-to-indentation)
+        (lua-find-regexp 'forward lua-indentation-modifier-regexp (line-end-position))
+        (back-to-indentation)
+        (let ((first-token-p (and (= (match-beginning 0) (point))
+                                  (= (lua-find-regexp 'forward lua-indentation-modifier-regexp (line-end-position)) cur-point))))
+          (lua-goto-matching-block-token found-pos 'backward)
+          (if (and (/= line (line-number-at-pos)) (not first-token-p))
+              (cons 'relative (- lua-indent-level))
+            (cons 'remove-matching 0))))))
 
    ;; Everything else. This is from the original code: If opening a block
    ;; (match-data 1 exists), then push indentation one level up, if it is
@@ -1326,47 +1318,16 @@ Return list of indentation modifiers from point to BOUND."
   indentation-info)
 
 
+
 (defun lua-calculate-indentation-info (&optional parse-end)
   "For each block token on the line, computes how it affects the indentation.
 The effect of each token can be either a shift relative to the current
 indentation level, or indentation to some absolute column. This information
 is collected in a list of indentation info pairs, which denote absolute
 and relative each, and the shift/column to indent to."
-  (let ((combined-line-end (line-end-position))
-        indentation-info)
-
-    (while (lua-is-continuing-statement-p)
-      (lua-forward-line-skip-blanks 'back))
-
-    ;; calculate indentation modifiers for the line itself
-    (setq indentation-info (list (cons 'absolute (current-indentation))))
-
-    (back-to-indentation)
-    (setq indentation-info
-          (lua-calculate-indentation-info-1
-           indentation-info (min parse-end (line-end-position))))
-
-    ;; and do the following for each continuation line before PARSE-END
-    (while (and (eql (forward-line 1) 0)
-                (<= (point) parse-end))
-
-      ;; handle continuation lines:
-      (if (lua-is-continuing-statement-p)
-          ;; if it's the first continuation line, add one level
-          (unless (eq (car (car indentation-info)) 'continued-line)
-            (push (cons 'continued-line lua-indent-level) indentation-info))
-
-        ;; if it's the first non-continued line, subtract one level
-        (when (eq (car (car indentation-info)) 'continued-line)
-          (pop indentation-info)))
-
-      ;; add modifiers found in this continuation line
-      (setq indentation-info
-            (lua-calculate-indentation-info-1
-             indentation-info (min parse-end (line-end-position)))))
-
-    indentation-info))
-
+  (lua-calculate-indentation-info-1
+   (list (cons 'absolute (current-indentation)))
+   (min parse-end (line-end-position))))
 
 (defun lua-accumulate-indentation-info (info)
   "Accumulates the indentation information previously calculated by
@@ -1479,35 +1440,48 @@ left-shifter expression. "
 
 
 
+
 (defun lua-calculate-indentation-override (&optional parse-start)
   "Return overriding indentation amount for special cases.
-
-If there's a sequence of block-close tokens starting at the
-beginning of the line, calculate indentation according to the
-line containing block-open token for the last block-close token
-in the sequence.
-
-If not, return nil."
-  (let (case-fold-search token-info block-token-pos)
+Look for an uninterrupted sequence of block-closing tokens that starts
+at the beginning of the line. For each of these tokens, shift indentation
+to the left by the amount specified in lua-indent-level."
+  (let ((indentation-modifier 0)
+        (indentation-info (list (cons 'absolute 0)))
+        (case-fold-search nil))
     (save-excursion
       (if parse-start (goto-char parse-start))
-
+      ;; Look for the last block closing token
       (back-to-indentation)
-      (unless (lua-comment-or-string-p)
-        (while
-            (and (looking-at lua-indentation-modifier-regexp)
-                 (setq token-info (lua-get-block-token-info (match-string 0)))
-                 (not (eq 'open (lua-get-token-type token-info))))
-          (setq block-token-pos (match-beginning 0))
-          (goto-char (match-end 0))
-          (skip-syntax-forward " " (line-end-position)))
+      (when (and (not (lua-comment-or-string-p))
+                 (looking-at lua-indentation-modifier-regexp)
+                 (let ((token-info (lua-get-block-token-info (match-string 0))))
+                   (and token-info
+                        (not (eq 'open (lua-get-token-type token-info))))))
+        ;; Compute indentation changes induced by unmatched closers on current line.
+        (while (lua-find-regexp 'forward lua-indentation-modifier-regexp (line-end-position))
+          (when (and (not (lua-comment-or-string-p))
+                     (looking-at lua-indentation-modifier-regexp)
+                     (let ((token-info (lua-get-block-token-info (match-string 0))))
+                       (and token-info
+                            (not (eq 'open (lua-get-token-type token-info))))))
+            (setq indentation-modifier (- indentation-modifier lua-indent-level))))
+        ;; Current line indentation is:
+        ;; + indentation changes induced by unmatched closers on current line
+        ;; + indentation changes induced by previous line
+        ;; + previous line indentation
+        ;; - indent-level if previous line was a continuation.
+        ;; - indent-level
+        (lua-forward-line-skip-blanks 'back)
+        (- (+ indentation-modifier
+              (cdr (lua-accumulate-indentation-info (lua-calculate-indentation-info-1 indentation-info (line-end-position))))
+              (current-indentation))
+           ;; Previous line is a continuing statement, but not current.
+           (if (lua-is-continuing-statement-p)
+               lua-indent-level
+             0)
+           lua-indent-level)))))
 
-        (when (lua-goto-matching-block-token block-token-pos 'backward)
-          ;; Exception cases: when the start of the line is an assignment,
-          ;; go to the start of the assignment instead of the matching item
-          (if (lua-point-is-after-left-shifter-p)
-              (current-indentation)
-            (current-column)))))))
 
 (defun lua-calculate-indentation (&optional parse-start)
   "Return appropriate indentation for current line as Lua code."
@@ -1515,19 +1489,29 @@ If not, return nil."
     (let ((continuing-p (lua-is-continuing-statement-p))
           (cur-line-begin-pos (line-beginning-position)))
       (or
-       ;; when calculating indentation, do the following:
-       ;; 1. check, if the line starts with indentation-modifier (open/close brace)
-       ;;    and if it should be indented/unindented in special way
+       ;; First, check if the line starts with a closer. If so, it should be
+       ;; indented/unindented in special way
        (lua-calculate-indentation-override)
 
        (when (lua-forward-line-skip-blanks 'back)
-         ;; the order of function calls here is important. block modifier
-         ;; call may change the point to another line
-         (let* ((modifier
+         ;; The order of function calls here is important. Block modifier
+         ;; call may change the point to another line.
+         (let ((modifier
                  (lua-calculate-indentation-block-modifier cur-line-begin-pos)))
-           (+ (current-indentation) modifier)))
+           (+ (current-indentation)
+              modifier
 
-       ;; 4. if there's no previous line, indentation is 0
+              ;; Previous line is a continuing statement, but not current.
+              (if (and (lua-is-continuing-statement-p) (not continuing-p))
+                  (- lua-indent-level)
+                0)
+              
+              ;; Current line is a continuing statement, but not previous.
+              (if (and (not (lua-is-continuing-statement-p)) continuing-p)
+                  lua-indent-level
+                0))))
+
+       ;; If there's no previous line, indentation is 0.
        0))))
 
 (defun lua-beginning-of-proc (&optional arg)
