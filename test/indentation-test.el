35a650dc71fffc8a4603c4df4d2e1649fdfe9647
Pierre Neidhardt <ambrevar@gmail.com>
2014-11-07 15:34:43 +0100

    test: Adapt issue 33

--- test/indentation-test.el
+++ test/indentation-test.el
@@ -72,11 +72,11 @@ bar = 20"))
   (should-lua-indent "\
 a =
    {
-   }
+}
 
 b =
    {
-   },
+},
 
 
 a = {
@@ -92,23 +92,23 @@ this_should_be_unindented()
 -- here foobar should be indented as simple continuation statement
 a = a +
    dosmth(
-   ) +
+) +
    foobar
 
 a =
    do_smth(
-      do_smth_arg
-   )
+   do_smth_arg
+)
 
 b =
    {
-      table_elt0_indented,
-      table_elt1_indented
-   }
+   table_elt0_indented,
+   table_elt1_indented
+}
 
 this_should_be_unindented_too =
    {
-   }
+}
 
 this_should_be_unindented_three = etc"))
 
